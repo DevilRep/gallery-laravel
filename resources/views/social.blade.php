491818ef71f2fb4414b7b7b909385869a88dac99
DevilRep <flaringrep@gmail.com>
2018-12-18 16:35:32 +0200

    D3: Added footer

---
+++ resources/views/social.blade.php
@@ -0,0 +1,16 @@
+<div class="social">
+    <ul>
+        <li>
+            <a href="#"><i class="fab fa-facebook-square"></i></a>
+        </li>
+        <li>
+            <a href="#"><i class="fab fa-twitter-square"></i></a>
+        </li>
+        <li>
+            <a href="#"><i class="fab fa-instagram"></i></a>
+        </li>
+        <li>
+            <a href="#"><i class="fab fa-github-square"></i></a>
+        </li>
+    </ul>
+</div>
