5b530ff7b2b973c966c6138fa4fbd33be9b008c1
DevilRep <flaringrep@gmail.com>
2018-12-18 14:53:56 +0200

    D3: Added a main layout and header

---
+++ resources/views/header.blade.php
@@ -0,0 +1,13 @@
+<header>
+    <nav>
+        <div class="title-wrapper">
+            <h1><a href="/" title="Gallery" rel="home">Gallery</a></h1>
+        </div>
+        <div class="menu-wrapper">
+            <ul>
+                <li><a href="/">Home</a></li>
+                <li><a href="#">Categories</a></li>
+            </ul>
+        </div>
+    </nav>
+</header>
